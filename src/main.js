2281b719a292d86760908b4f3639f5d9a0f1ffec
ruizhixiaochen <2574481749@qq.com>
2023-01-19 12:30:18 +0800

    初始化vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vant from 'vant'
+import 'vant/lib/index.css'
+import './styles/index.less'
 
+Vue.use(vant)
 Vue.config.productionTip = false
 
 new Vue({
